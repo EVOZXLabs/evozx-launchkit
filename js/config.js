bb41cb4ab21a6335eda4c6fb8c63279477a34fe9
EVOZXLabs <agustriyadiofficial@gmail.com>
2026-06-05 01:45:53 +0700

    Refactor CONFIG and update ABI function signatures

--- js/config.js
+++ js/config.js
@@ -1,34 +1,12 @@
-const CONFIG = {
-
-    FACTORY_ADDRESS:
-    "0x3F810a44D29a4f0fF7880641E69EBCBc076dA220",
-
-    CHAIN_ID:
-    805,
-
-    CHAIN_NAME:
-    "Evoz Mainnet",
-
-    RPC_URL:
-    "https://rpc.evozscan.com",
-
-    EXPLORER_URL:
-    "https://evozscan.com",
-
-    CURRENCY_SYMBOL:
-    "EVOZ"
-
-};
-
 const FACTORY_ABI = [
 
     "function createToken(string name_, string symbol_, uint256 supply_) external returns(address)",
 
     "function allTokens(uint256) view returns(address,address,string,string,uint256,uint256)",
 
-    "function getToken(uint256) view returns(address,address,string memory,string memory,uint256,uint256)",
+    "function getToken(uint256) view returns(address,address,string,string,uint256,uint256)",
 
-    "function allTokensLength() view returns(uint256)",
+    "function totalTokens() view returns(uint256)",
 
     "event TokenCreated(address indexed creator,address indexed token,string name,string symbol,uint256 supply)"
 
