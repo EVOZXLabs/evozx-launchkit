3fbeaaa6c98aa22bd2f6478f9a401eee61c2e870
EVOZXLabs <agustriyadiofficial@gmail.com>
2026-06-06 10:21:58 +0700

    Add paused function to config.js

--- js/config.js
+++ js/config.js
@@ -211,6 +211,20 @@ const EXCHANGE_ABI = [
     ],
     "stateMutability": "view",
     "type": "function"
+},
+
+{
+    "inputs": [],
+    "name": "paused",
+    "outputs": [
+        {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
 }
 
 ];
